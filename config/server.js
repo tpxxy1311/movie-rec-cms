be30b7320b7acfb55166ef0c454580491d3f1674
tpxxy1311 <65901921+tpxxy1311@users.noreply.github.com>
2022-08-25 09:07:35 +0200

    db

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '04ba2df0648daa808bd2fa43e3283d55'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd2ad5a6b-2c78-4776-8bbd-dc27f4d7fb68'
+};
