0bcca20cd676087599e52a27caedeb652d9ae787
tpxxy1311 <65901921+tpxxy1311@users.noreply.github.com>
2022-08-25 10:10:49 +0200

    db config

--- config/database.js
+++ config/database.js
@@ -4,17 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        uri: env('DATABASE_URI'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: true,
       },
     },
   },
 });
+ 
